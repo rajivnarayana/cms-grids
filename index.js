bff5acd12212a960cb61a023d56704d24120e6a9
Rajiv Narayana <rajiv@webileapps.com>
2016-07-18 19:08:52 +0530

    Added tsconfig.json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const jade = require("jade");
 const path_1 = require("path");
 function render(req, res, next) {
     if (res.grid) {
-        res.html = jade.renderFile(path_1.join(__dirname, 'grid.jade'), { grid: res.grid });
+        res.html = { content: jade.renderFile(path_1.join(__dirname, 'grid.jade'), { grid: res.grid }) };
     }
     return next();
 }
